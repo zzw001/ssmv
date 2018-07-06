2abe5e50734932937b37db67d96c82fe18448b57
ecaps28 <2113013324@qq.com>
2018-07-06 16:36:58 +0800

    NEW2

--- ssmv-common/ssmv-common-web/src/test/java/test/UserTest.java
+++ ssmv-common/ssmv-common-web/src/test/java/test/UserTest.java
@@ -22,7 +22,7 @@ public class UserTest {
         User user = new User();
         user.setUuid(UUID.randomUUID().toString());
         user.setName("3");
-        user.setPassword("1");
+        user.setPassword("0");
         userService.add(user);
     }
 
