3cf4e5185721476d5c3d0cc7a599beae4517b3f9
zzw <2218904390@qq.com>
2018-07-06 16:32:24 +0800

    冲突解决

--- ssmv-common/ssmv-common-web/src/test/java/test/UserTest.java
+++ ssmv-common/ssmv-common-web/src/test/java/test/UserTest.java
@@ -28,7 +28,7 @@ public class UserTest {
 
     @Test
     public void nameExistTest(){
-        String name = "10000";
+        String name = "100000";
         System.out.println(userService.isExistName(name));
     }
 }
