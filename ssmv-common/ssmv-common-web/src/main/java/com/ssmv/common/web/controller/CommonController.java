c66ec0bc9bc45772b040c0c35405d59aa1c13bde
zzw <2218904390@qq.com>
2018-07-11 15:56:07 +0800

    后台程序

--- ssmv-common/ssmv-common-web/src/main/java/com/ssmv/common/web/controller/CommonController.java
+++ ssmv-common/ssmv-common-web/src/main/java/com/ssmv/common/web/controller/CommonController.java
@@ -5,6 +5,7 @@ import com.ssmv.common.model.NameMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
@@ -15,6 +16,7 @@ public class CommonController {
     private UserService userService;
 
     @RequestMapping("/register/nameExist")
+    @ResponseBody
     public NameMessage nameMessage(@RequestParam("username")String username){
         return userService.isExistName(username);
     }
